302a3c5e40b664a6b31876e3aa438a3d4c82415a
diogenes-omar-lora <omarlora08@hotmail.com>
2023-03-08 22:33:34 -0400

    menu

--- restaurant/src/app/app.module.ts
+++ restaurant/src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { MainComponent } from './main/main.component';
+import { FooterComponent } from './footer/footer.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { WidgetComponent } from './widget/widget.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    MainComponent,
+    FooterComponent,
+    SidebarComponent,
+    WidgetComponent
   ],
   imports: [
     BrowserModule
